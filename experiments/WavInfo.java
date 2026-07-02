55d3b539df59ee8ac3da90ce711a9ceca694826f
Krish Kumar <krishkumarusa10@gmail.com>
2026-07-01 22:35:28 -0700

    Began work on WAV file info script

---
+++ experiments/WavInfo.java
@@ -0,0 +1,106 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public class WavInfo {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        System.out.print("Enter in the WAV file you want to analyze: ");
+        String filename = read.nextLine().trim();
+
+        if(!filename.endsWith(".wav")) {
+            System.err.println("Error: File provided was not a WAV file!");
+            System.exit(1);
+        }
+
+        byte[] headerBytes = new byte[80];
+        File f = new File(filename);
+        try(FileInputStream in = new FileInputStream(f)) {
+            in.read(headerBytes);
+        }
+        catch(IOException e) {
+            System.err.println("Error: Could not read file: " + filename);
+            System.exit(1);
+        }
+
+        //Bytes 0-3 are the RIFF marker
+        String riffMarker = new String(headerBytes, 0, 4);
+        if(riffMarker.equals("RIFF")) {
+            System.out.println("RIFF marker found");
+        }
+        else {
+            System.err.println("Error: Not a valid RIFF file!");
+            System.exit(1);
+        }
+
+        //Bytes 4-7 are the size bytes (minus 8 bytes) in Little Endian
+        int sizeFromHeader = readInt(headerBytes, 4);
+        int actualSize = sizeFromHeader + 8;
+        System.out.println("File size from header: " + sizeFromHeader + " bytes");
+        System.out.println("Actual file size: " + actualSize + " bytes");
+
+        //Bytes 8-11 are the WAVE marker
+        String waveMarker = new String(headerBytes, 8, 4);
+        if(waveMarker.equals("WAVE")) {
+            System.out.println("WAVE marker found");
+        }
+        else {
+            System.err.println("Error: Not a valid WAVE file!");
+            System.exit(1);
+        }
+
+        //Bytes 12-15 are the format chunk marker
+        String fmtMarker = new String(headerBytes, 12, 4);
+        if(fmtMarker.equals("fmt ")) {
+            System.out.println("Format chunk found");
+        }
+        else {
+            System.err.println("Error: Format chunk not found!");
+            System.exit(1);
+        }
+
+        //Bytes 16-19 are the format chunk size in Little Endian
+        int fmtChunkSize = readInt(headerBytes, 16);
+        System.out.println("Format chunk size: " + fmtChunkSize + " bytes");
+
+        //Bytes 20-21 are the format type
+        short fmtType = readShort(headerBytes, 20);
+        System.out.println("Format type: " + fmtType);
+
+        //Bytes 22-23 are the number of channels used
+        short numChannels = readShort(headerBytes, 22);
+        System.out.println("Number of channels: " + numChannels);
+
+        //Bytes 24-27 are the sample rate
+        int sampleRate = readInt(headerBytes, 24);
+        System.out.println("Sample rate: " + sampleRate + " Hz");
+
+        //Bytes 28-31 are the byte rate
+        int byteRate = readInt(headerBytes, 28);
+        System.out.println("Byte rate: " + byteRate + " bytes/sec");
+
+        //Bytes 32-33 are the block align (bytes/sample)
+        short blockAlign = readShort(headerBytes, 32);
+        System.out.println("Block align: " + blockAlign + " bytes/sample");
+
+        //Bytes 34-35 are the bits per sample (audio resolution)
+        short bitsPerSample = readShort(headerBytes, 34);
+        System.out.println("Bits per sample: " + bitsPerSample + "-bit");
+    }
+
+    public static int readInt(byte[] arr, int offset) {
+        return ByteBuffer.wrap(arr, offset, 4)
+                .order(ByteOrder.LITTLE_ENDIAN)
+                .getInt();
+    }
+
+    public static short readShort(byte[] arr, int offset) {
+        return ByteBuffer.wrap(arr, offset, 2)
+                .order(ByteOrder.LITTLE_ENDIAN)
+                .getShort();
+    }
+}
